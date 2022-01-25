28909b7bbffa22bfdc18e6ec7ee5f31db977f7c6
Junsu Park <steve7867@naver.com>
2022-01-26 02:10:19 +0900

    링크 추가

--- src/SWEA/_9999/_2/Solution.java
+++ src/SWEA/_9999/_2/Solution.java
@@ -1,3 +1,7 @@
+/**
+ * 9999. 광고 시간 정하기
+ * https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXIvPBC6aqUDFAXR&categoryId=AXIvPBC6aqUDFAXR&categoryType=CODE&problemTitle=9999&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+ */
 package SWEA._9999._2;
 
 import java.io.*;
