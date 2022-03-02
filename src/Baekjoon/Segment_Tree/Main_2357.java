90d09d08f4319afcc9fe4211334b3ab4f21b4b58
Junsu Park <parkjunsu3@gmail.com>
2022-03-02 22:09:51 +0900

    백준 2357

---
+++ src/Baekjoon/Segment_Tree/Main_2357.java
@@ -0,0 +1,99 @@
+//https://www.acmicpc.net/problem/2357
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main_2357 {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final StringBuilder sb = new StringBuilder();
+	private static int n;
+	private static int m;
+	private static int[] arr;
+	private static int[] min;
+	private static int[] max;
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		makeTree();
+		processQuery();
+		System.out.println(sb);
+	}
+
+	private static void processQuery() throws IOException {
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = sttoi(st);
+			int b = sttoi(st);
+			sb.append(getMin(1, 1, n, a, b))
+			.append(' ')
+			.append(getMax(1, 1, n, a, b))
+			.append('\n');
+		}
+		
+	}
+
+	private static int getMax(int node, int start, int end, int l, int r) {
+		if (l > end || r < start)
+			return Integer.MIN_VALUE;
+		else if (l <= start && end <= r)
+			return max[node];
+		else
+			return Math.max(getMax(node * 2, start, (start + end) / 2, l, r)
+					, getMax(node * 2 + 1, (start + end) / 2 + 1, end, l, r));
+	}
+
+	private static int getMin(int node, int start, int end, int l, int r) {
+		if (l > end || r < start)
+			return Integer.MAX_VALUE;
+		else if (l <= start && end <= r)
+			return min[node];
+		else
+			return Math.min(getMin(node * 2, start, (start + end) / 2, l, r)
+					, getMin(node * 2 + 1, (start + end) / 2 + 1, end, l, r));
+	}
+
+	private static void makeTree() {
+		int height = logUp(n) + 1;
+		int treeSize = (1 << height) - 1;
+		min = new int[treeSize + 1];
+		max = new int[treeSize + 1];
+		
+		minInit(1, 1, n);
+		maxInit(1, 1, n);
+	}
+	
+	private static int maxInit(int node, int start, int end) {
+		if (start == end)
+			return max[node] = arr[start];
+		else
+			return max[node] = Math.max(maxInit(node * 2, start, (start + end) / 2)
+					, maxInit(node * 2 + 1, (start + end) / 2 + 1, end));
+	}
+
+	private static int minInit(int node, int start, int end) {
+		if (start == end)
+			return min[node] = arr[start];
+		else
+			return min[node] = Math.min(minInit(node * 2, start, (start + end) / 2)
+					, minInit(node * 2 + 1, (start + end) / 2 + 1, end));
+	}
+
+	private static int logUp(int n) {
+		return (int) Math.ceil(Math.log(n) / Math.log(2));
+	}
+
+	private static void input() throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = sttoi(st);
+		m = sttoi(st);
+		
+		arr = new int[n + 1];
+		for (int i = 1; i <= n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+	}
+
+	private static int sttoi(StringTokenizer st) {
+		return Integer.parseInt(st.nextToken());
+	}
+}
